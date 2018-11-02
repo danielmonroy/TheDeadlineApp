136d7636fb7ebfd58c5b929a1473cc595b0b30e4
Dan Monroy <danielmonroy@ciencias.unam.mx>
2018-11-01 23:28:14 -0600

    Updated logo, colors and gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :projects
-  devise_for :users
+  devise_for :users, path: 'auth'
   resources :dashboard
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "dashboard#index"
